9a6447d79ce220564fcc97d1debb24b2ec11ac2d
D3V-S4NJ4Y <sanjay.dev925@gmail.com>
2025-08-01 11:10:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,18 @@ echo "\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
+maxUploadSize = 50\n\
+\n\
+[browser]\n\
+gatherUsageStats = false\n\
+\n\
+[global]\n\
+disableWatchdogWarning = true\n\
+\n\
+[theme]\n\
+base = 'light'\n\
 \n\
 " > ~/.streamlit/config.toml
+
+# Create artifacts directory if it doesn't exist
+mkdir -p artifacts
